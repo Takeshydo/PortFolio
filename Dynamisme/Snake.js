1d34c3db5985afdc48e8733116c2577fb7a4d55a
MathysL <m.lacoque@ecole-ippsi.net>
2025-11-12 10:43:24 +0100

    Ajout déplacement du Snake

--- Dynamisme/Snake.js
+++ Dynamisme/Snake.js
@@ -8,6 +8,8 @@ var context;
 //snake head | start
 var snakeX = blockSize * 5;
 var snakeY = blockSize * 5;
+var velocityX = 0;
+var velocityY = 0;
 
 //food point 
 var FoodPX;
@@ -20,7 +22,9 @@ window.onload = function () {
     context = board.getContext("2d");//utiliser pour dessiner sur le board
 
     placeFood();
-    update();
+    document.AddEvenlisterner("keyup", changeDirection);
+    //update();
+    setInterval(update, 1000 / 10);
 
 }
 
@@ -29,6 +33,9 @@ function update() {
     context.fillRect(0, 0, board.width, board.height);
 
     context.fillStyle = "lime";
+    //affichage sur le canvas
+    snakeX += velocityX;
+    snakeY += velocityY;
     context.fillRect(snakeY, snakeX, blockSize, blockSize);
 
     context.fillStyle = "red";
@@ -40,4 +47,23 @@ function update() {
 function placeFood() {
     FoodPX = Math.floor((Math.random() * cols) * blockSize);
     FoodPY = Math.floor((Math.random() * rows) * blockSize);
+}
+
+function changeDirection(e) {
+    if (e.code == "ArrowUp") {
+        velocityX = 0;
+        velocityY = -1;
+    }
+    else if (e.code == "ArrowDown") {
+        velocityX = 0;
+        velocityY = 1;
+    }
+    else if (e.code == "ArrowLeft") {
+        velocityX = -1;
+        velocityY = 0;
+    }
+    else if (e.code == "ArrowRight") {
+        velocityX = 1;
+        velocityY = 0;
+    }
 }
